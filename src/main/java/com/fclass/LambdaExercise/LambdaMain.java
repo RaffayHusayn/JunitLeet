b261408d9dab49ead60a813147a0baa29290a101
Raffay Syed <raffayhusayn@gmail.com>
2022-03-18 18:22:31 -0400

    Add lambdas with Consumer functional interface to solve the task

--- src/main/java/com/fclass/LambdaExercise/LambdaMain.java
+++ src/main/java/com/fclass/LambdaExercise/LambdaMain.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class LambdaMain {
@@ -35,10 +37,31 @@ public class LambdaMain {
         //Task # 2: Just printing all the students, no need to use lambdas here
         studentList.forEach(System.out::println);
 
-        System.out.println("\n=======Students whose last Name start with \"C\"===========\n");
 
-        //Task # 3: Only printing where lastName starts with "C"
-        studentList.stream().filter(s->s.getLastName().startsWith("C")).forEach(System.out::println);
+        //Task # 3: Only printing where lastName starts with "C" using lambda in the filter method
+                /*
+                Predicate and Consumer are two of the Functional Interfaces provided by java
+                in the java.util.function package that we can use for the lambdas type definition
+                 */
+        Predicate<Student> nameStartC = s->s.getLastName().startsWith("C");
+
+        //this is the same as using .map(s->s.getFirstName()).forEach(System.out::println)
+        Consumer<Student> printFirstName = s-> System.out.println(s.getFirstName());
+
+        //this is the same as using forEach(System.out::println)
+        Consumer<Student> printFullStudent = s-> System.out.println(s.toString());
+
+        System.out.println("\n=======Students whose last Name start with \"C\"(using Consumer and Predicate Functional interfaces)===========\n");
+        System.out.println("-----First Names-----");
+        studentList.stream().filter(nameStartC).forEach(printFirstName);
+        System.out.println("-----Full Names-----");
+        studentList.stream().filter(nameStartC).forEach(printFullStudent);
+
+        System.out.println("\n =====Student whose last Name start with \"C\" (using method references and map() )=========\n");
+        System.out.println("-----First Names-----");
+        studentList.stream().filter(nameStartC).map(s->s.getFirstName()).forEach(System.out::println);
+        System.out.println("-----Full Names-----");
+        studentList.stream().filter(nameStartC).forEach(System.out::println);
 
     }
 }
