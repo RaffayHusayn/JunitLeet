94840f25c0a8753544b3614cc27a4515a9306873
Raffay Syed <raffayhusayn@gmail.com>
2022-03-18 23:38:01 -0400

    Add a new class using BiFunction functional interface for lambdas

--- src/main/java/com/fclass/lambda_exercise/LambdaWithBiFunction.java
+++ src/main/java/com/fclass/lambda_exercise/LambdaWithBiFunction.java
@@ -1,34 +1,16 @@
-package com.fclass.lambda_exception_handling;
+package com.fclass.lambda_exercise;
 
-import jdk.jshell.execution.JdiDefaultExecutionControl;
-
-import java.lang.reflect.Array;
-import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 
-public class LambdaExceptionHandling {
+public class LambdaWithBiFunction {
     public static void main(String[] args){
         int[] numArray = {0,1,2,3,4,5,6};
-        int target = 0;
-        BiConsumer<Integer, Integer> addProcessLambda = (num1, num2 )-> System.out.println(num1+num2);
-        BiConsumer<Integer, Integer> subtractProcessLambda = (num1, num2 )-> System.out.println(num1-num2);
-        BiConsumer<Integer, Integer> multiplyProcessLambda = (num1, num2 )-> System.out.println(num1*num2);
-        BiConsumer<Integer, Integer> divideProcessLambda = (num1, num2 )-> System.out.println(num1/num2);
-        System.out.println("+++++++++ Addition Process ++++++++++++++");
-        processConsumer(numArray, target, addProcessLambda);
-        System.out.println("-------- Subtract Process ---------------");
-        processConsumer(numArray, target, subtractProcessLambda);
-        System.out.println("xxxxxxxx Multiply Process xxxxxxxxxxxxxxxx");
-        processConsumer(numArray, target, multiplyProcessLambda);
-        System.out.println("//////// Divide Process /////////////////");
-        processConsumer(numArray, target, divideProcessLambda);
-
-
+        int target = 2;
         System.out.println("==========================================\n===========================================");
         System.out.println(" Using Bi Function which take 2 things in and return 1 thing out");
         System.out.println("==========================================\n===========================================\n");
 
-        BiFunction<Integer, Integer, Integer> addFunction = (num1,num2)->num1+num2;
+        BiFunction<Integer, Integer, Integer> addFunction = (num1, num2)->num1+num2;
         BiFunction<Integer, Integer, Integer> subtractFunction = (num1,num2)->num1-num2;
         BiFunction<Integer, Integer, Integer> multiplyFunction = (num1,num2)->num1*num2;
         BiFunction<Integer, Integer, Integer> divideFunction = (num1,num2)->num1/num2;
@@ -46,11 +28,7 @@ public class LambdaExceptionHandling {
         int[] divideFunctionArray = processFunction(numArray, target, divideFunction);
         for(int n:divideFunctionArray) System.out.println(n);
     }
-    public static void processConsumer(int[] numArray, int target , BiConsumer<Integer, Integer> biConsumer){
-        for(int n: numArray){
-            biConsumer.accept(n, target);
-        }
-    }
+
     public static int[] processFunction(int[] numArray, int target , BiFunction<Integer, Integer, Integer> biFunction){
         int[] processResultArray = new int[numArray.length];
         for(int i=0; i < numArray.length; i++){
