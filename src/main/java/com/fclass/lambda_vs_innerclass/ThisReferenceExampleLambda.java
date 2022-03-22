4c2ba2ff99f2a4b9fd826698109f591d959fadaa
Raffay Syed <raffayhusayn@gmail.com>
2022-03-22 00:21:24 -0400

    Add how 'this' works in Lambdas

--- src/main/java/com/fclass/lambda_vs_innerclass/ThisReferenceExampleLambda.java
+++ src/main/java/com/fclass/lambda_vs_innerclass/ThisReferenceExampleLambda.java
@@ -1,5 +1,22 @@
 package com.fclass.lambda_vs_innerclass;
 
 public class ThisReferenceExampleLambda {
+    public void doSomething(SomeFunctionalInterface sfi){
+        sfi.something();
+    }
+    public void doSomethingCaller(ThisReferenceExampleLambda obj){
+        obj.doSomething(()-> System.out.println(this));
+    }
+    public static void main(String[] args){
+        ThisReferenceExampleLambda thisReferenceExampleLambda = new ThisReferenceExampleLambda();
+        //very strange code, probably a really bad way to do something
+        thisReferenceExampleLambda.doSomethingCaller(thisReferenceExampleLambda);
+    }
+    public String toString(){
+        return "Changing 'this' reference of the Object of class 'ThisReferenceExampleLambda'";
+    }
 
+}
+interface SomeFunctionalInterface2{
+    void something();
 }
