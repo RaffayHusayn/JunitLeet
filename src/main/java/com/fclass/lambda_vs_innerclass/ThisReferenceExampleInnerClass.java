fc46c6485ab584d4d1f3204612a9f2d25d04c335
Raffay Syed <raffayhusayn@gmail.com>
2022-03-21 23:52:55 -0400

    No functional changes: Added comments to understand what's happening

--- src/main/java/com/fclass/lambda_vs_innerclass/ThisReferenceExampleInnerClass.java
+++ src/main/java/com/fclass/lambda_vs_innerclass/ThisReferenceExampleInnerClass.java
@@ -2,11 +2,14 @@ package com.fclass.lambda_vs_innerclass;
 
 public class ThisReferenceExampleInnerClass {
 
+    //some function that uses Functional Interface as an argument:
+    // we can define this Interface using 1. Regular Class 2. Anonymous Inner class 3. Lambda function
     public void doSomething(SomeFunctionalInterface sfi){
         sfi.something();
     }
     public static void main(String[] args){
         ThisReferenceExampleInnerClass thisReferenceExampleInnerClass = new ThisReferenceExampleInnerClass();
+        System.out.println("can't access the value of 'this' in the static function");
         thisReferenceExampleInnerClass.doSomething(new SomeFunctionalInterface() {
             @Override
             public void something() {
