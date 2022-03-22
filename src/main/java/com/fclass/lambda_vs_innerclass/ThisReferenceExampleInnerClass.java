01c742c3448107454e210eac998703763d1abcde
Raffay Syed <raffayhusayn@gmail.com>
2022-03-21 23:50:31 -0400

    Add code to print value of 'this' inside the anonymous inner class which reference the object of the interface that the inner class is based on

--- src/main/java/com/fclass/lambda_vs_innerclass/ThisReferenceExampleInnerClass.java
+++ src/main/java/com/fclass/lambda_vs_innerclass/ThisReferenceExampleInnerClass.java
@@ -2,7 +2,17 @@ package com.fclass.lambda_vs_innerclass;
 
 public class ThisReferenceExampleInnerClass {
 
+    public void doSomething(SomeFunctionalInterface sfi){
+        sfi.something();
+    }
     public static void main(String[] args){
+        ThisReferenceExampleInnerClass thisReferenceExampleInnerClass = new ThisReferenceExampleInnerClass();
+        thisReferenceExampleInnerClass.doSomething(new SomeFunctionalInterface() {
+            @Override
+            public void something() {
+                System.out.println("Value of this in the Anonymous Inner class : \"" + this + "\" reference the object of the Interface");
+            }
+        });
 
     }
 
